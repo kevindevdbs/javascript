5a38341e2cf4a91c4218e2d36049bbecf71bc7f9
Kevin Dias <kevindevdbs@gmail.com>
2024-10-22 20:32:13 -0300

    att

--- Desafios/Desafio 1/script.js
+++ Desafios/Desafio 1/script.js
@@ -1,1 +1,21 @@
-alert("Olá!")
+function carregar() {
+    var data = new Date();
+    var hora = data.getHours();
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
+
+    msg.innerHTML = `Agora são ${hora} Horas.`;
+
+    if (hora >= 0 && hora < 12) {
+        img.src = 'manhaedit.jpg';
+        document.body.style.backgroundColor = '#e2cd9f';
+
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'tardeedit.jpg';
+        document.body.style.backgroundColor = ('#b9846f')
+
+    } else {
+        img.src = 'noiteedit.jpg';
+        document.body.style.backgroundColor = 'black';
+    }
+}
