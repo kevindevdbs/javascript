c513afc58b352373cec62173d9fe7d8613feae39
Kevin Dias <kevindevdbs@gmail.com>
2024-10-24 10:03:15 -0300

    att

---
+++ Exercicios/Exercicio 3/script.js
@@ -0,0 +1,11 @@
+var inical = window.document.getElementById('inicial')
+var final = window.document.getElementById('final')
+var passos = window.document.getElementById('passo')
+
+function contar() {
+    if (inical.value.length == 0 || final.value.length == 0 || passos.value.length == 0) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    }
+    
+    for
+}
